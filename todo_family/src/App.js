64486e0855f2fb21fa8ae5551d982a367a04d5cf
jinho6225 <jinho6225@hotmail.com>
2020-08-10 00:07:42 -0700

    css added and added link anchor tag

--- todo_family/src/App.js
+++ todo_family/src/App.js
@@ -9,13 +9,12 @@ const App = (props) => {
 
         {todoList.map(todo => {
           const { id, title, description, github, live, img } = todo
-          console.log(img)
           return (
-              <div className="col mb-4" key={id}>
+              <div className="col mb-4 cardDiv" key={id}>
                 <div className="card">
-                  <img src={img} className="card-img-top" alt="..." />
+                  <a href={live} target="_blank"><img src={img} className="card-img-top" alt="..." /></a>
                   <div className="card-body">
-                    <h5 className="card-title">{title} <a href={github} target="_blank" className="github"><i class="fab fa-github-square fa-lg"></i></a></h5>
+                    <h5 className="card-title">{title} <a href={github} target="_blank" className="github"><i className="fab fa-github-square fa-lg"></i></a></h5>
                     <p className="card-text">{description}</p>
                   </div>
                 </div>
