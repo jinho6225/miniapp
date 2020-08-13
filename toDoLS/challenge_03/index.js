5e9222d6846e10a4d5094b41157527aff045db46
jinho6225 <jinho6225@hotmail.com>
2020-08-12 22:50:33 -0700

    structure done

--- toDoLS/challenge_03/index.js
+++ toDoLS/challenge_03/index.js
@@ -1,22 +1,45 @@
 let numbers = document.querySelectorAll('.num')
 const field = document.querySelector('.field')
+let operator = document.querySelectorAll('.operator')
+const reset = document.querySelector('.reset')
 
+function operatorHandleChange(e) {
+    const target = e.target
+    const operator = target.innerHTML
+    console.log(operator)
+    //if clicked operator, 
+    //then number on field need to be stored at some place
+    //and need to check which operator
+}
 
-function handleChange(e) {
+function numHandleChange(e) {
     const target = e.target
     const number = target.innerHTML
-    field.innerHTML += number
-    console.log(field.innerHTML)
+    if (field.innerHTML == 0) {
+        field.innerHTML = number
+    } else {
+        field.innerHTML += number
+    }
 }
 function numberFn() {
     numbers = Array.from(numbers)
+    operator = Array.from(operator)
     for (let i = 0; i < numbers.length; i++) {
-        numbers[i].addEventListener('click', handleChange)
+        numbers[i].addEventListener('click', numHandleChange)
     }
+    for (let i = 0; i < operator.length; i++) {
+        operator[i].addEventListener('click', operatorHandleChange)
+    }
+}
+
+function resetHandler() {
+    field.innerHTML = 0;
+    //clear storage
 }
 
 function init() {
     numberFn()
+    reset.addEventListener('click', resetHandler)
 }
 
 init()
