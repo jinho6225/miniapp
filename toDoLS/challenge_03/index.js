4032e170c54027253d52a89ff90edaca0c5f37b2
jinho6225 <jinho6225@hotmail.com>
2020-08-13 10:16:36 -0700

    basic calculate done

--- toDoLS/challenge_03/index.js
+++ toDoLS/challenge_03/index.js
@@ -1,25 +1,80 @@
 let numbers = document.querySelectorAll('.num')
 const field = document.querySelector('.field')
 const equal = document.querySelector('.equal')
-let operator = document.querySelectorAll('.operator')
+let operators = document.querySelectorAll('.operator')
 const reset = document.querySelector('.reset')
-let activeClass = document.querySelectorAll('.active')
+let operator = null
+let numA = null
+let numB = null
+let storeNumber = ''
+let result = null
 
-function numberHandle(e) {
+function numberHandler(e) {
     const num = e.target.innerHTML
     if(field.innerHTML == 0) {
-        field.innerHTML = num
+        storeNumber = num
     } else {
-        field.innerHTML += num
+        storeNumber += num
     }
+    numA = storeNumber
+    field.innerHTML = numA
 }
-function numberFn() {
+
+function resetHandler() {
+    field.innerHTML = 0
+    numA = null
+    numB = null
+    storeNumber = ''
+    operator = null
+    result = null
+}
+
+function operatorHandler(e) {
+    operator = e.target.innerHTML    
+    numB = numA
+    numA = null
+    storeNumber = ''
+}
+
+function equalHandler() {
+    const expr = operator;
+    switch (expr) {
+    case '+':
+        result = Number(numB) + Number(numA)
+        break;
+    case '-':
+        result = Number(numB) - Number(numA)
+        break;
+    case '*':
+        result = Number(numB) * Number(numA)
+        break;
+    case '/':
+        result = Number(numB) / Number(numA)
+        break;
+    default:
+        result = 0
+    }
+    field.innerHTML = result
+}
+
+
+
+
+
+
+function eventProvider() {
     for (let i = 0; i < numbers.length; i++) {
-        numbers[i].addEventListener('click', numberHandle)
+        numbers[i].addEventListener('click', numberHandler)
     }
+    for (let i = 0; i < operators.length; i++) {
+        operators[i].addEventListener('click', operatorHandler)
+    }
+    reset.addEventListener('click', resetHandler)
+    equal.addEventListener('click', equalHandler)
 }
+
 function init() {
-    numberFn()
+    eventProvider()
 }
 
 init()
