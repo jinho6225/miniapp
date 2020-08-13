f63fc0052ae5e0eb2cc63789c2e37466bf077bdc
jinho6225 <jinho6225@hotmail.com>
2020-08-13 09:19:46 -0700

    added :active css

--- toDoLS/challenge_03/index.js
+++ toDoLS/challenge_03/index.js
@@ -3,104 +3,23 @@ const field = document.querySelector('.field')
 const equal = document.querySelector('.equal')
 let operator = document.querySelectorAll('.operator')
 const reset = document.querySelector('.reset')
-let numA = null
-let numB = null
-let operatorA = null
-let result = null
+let activeClass = document.querySelectorAll('.active')
 
-
-function operatorHandleChange(e) {
-    const target = e.target
-    const operator = target.innerHTML
-        if (numB && numA) {
-            switch (operator) {
-                case '+':
-                    result = Number(numB) + Number(numA);
-                    break;
-                case '-':
-                    result = Number(numB) - Number(numA);
-                    break;
-                case '*':
-                    result = Number(numB) * Number(numA);
-                    break;
-                case '/':
-                    result = Number(numB) / Number(numA);
-                    break;
-                default:
-                    result = 0;
-                }
-                field.innerHTML = result;
-                numB = result
-                numA = null
-                operatorA = operator
-        } else {
-            numB = numA
-            numA = null
-            operatorA = operator
-        }
-}
-
-function numHandleChange(e) {
-    const target = e.target
-    const number = target.innerHTML
-    if (numA === null) {
-        numA = number
+function numberHandle(e) {
+    const num = e.target.innerHTML
+    if(field.innerHTML == 0) {
+        field.innerHTML = num
     } else {
-        if (numA == 0) {
-            numA = number
-        } else {
-            numA += number
-        }
+        field.innerHTML += num
     }
-    field.innerHTML = numA
 }
 function numberFn() {
-    numbers = Array.from(numbers)
-    operator = Array.from(operator)
     for (let i = 0; i < numbers.length; i++) {
-        numbers[i].addEventListener('click', numHandleChange)
-    }
-    for (let i = 0; i < operator.length; i++) {
-        operator[i].addEventListener('click', operatorHandleChange)
+        numbers[i].addEventListener('click', numberHandle)
     }
 }
-
-function resetHandler() {
-    field.innerHTML = 0;
-    numA = null
-    numB = null
-    operatorA = null
-    result = null
-}
-
-function equalHandler() {
-    if (result) {
-        console.log('heyhe')
-        numB = result
-    }
-    switch (operatorA) {
-    case '+':
-        result = Number(numB) + Number(numA);
-        break;
-    case '-':
-        result = Number(numB) - Number(numA);
-        break;
-    case '*':
-        result = Number(numB) * Number(numA);
-        break;
-    case '/':
-        result = Number(numB) / Number(numA);
-        break;
-    default:
-        result = 0;
-    }
-    field.innerHTML = result;
-}
-
 function init() {
     numberFn()
-    reset.addEventListener('click', resetHandler)
-    equal.addEventListener('click', equalHandler)
 }
 
 init()
